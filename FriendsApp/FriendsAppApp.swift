72047ca6a0560e5de2a0c4ed033f0390a1424d8c
tanvirgeekios <tanvir.alam@iosventure.com>
2021-05-23 10:52:12 +0600

    Initial Commit

---
+++ FriendsApp/FriendsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppApp.swift
+//  FriendsApp
+//
+//  Created by MD Tanvir Alam on 23/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
